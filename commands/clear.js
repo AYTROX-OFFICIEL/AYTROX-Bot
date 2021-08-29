0e2827757ce0793fb839d0691262d5e56f9e60c1
AYTROX <74268308+AYTROX-OFFICIEL@users.noreply.github.com>
2021-08-29 21:42:36 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -7,5 +7,6 @@ module.exports = {
       if (!/\d+/.test(count)) return message.channel.send('Veuillez indiquer un nombre de message à supprimer.')
       if (count < 1 || count > 99) return message.channel.send('Le nombre de message doit être compris entre 1 et 99!')
       const { size } = await message.channel.bulkDelete(Number(count) + 1, true)
+      message.member.send(`J'ai supprimé \`${size - 1}\` message(s) !`)
   }
 }
